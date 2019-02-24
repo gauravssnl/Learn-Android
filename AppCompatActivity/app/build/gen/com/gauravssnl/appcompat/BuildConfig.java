54604613b328a9135944a6d464d67ca16011fbfb
gauravssnl <gaurav.ssnl@gmail.com>
2019-02-24 11:36:08 +0530

    AppCompatActivity added

---
+++ AppCompatActivity/app/build/gen/com/gauravssnl/appcompat/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.gauravssnl.appcompat;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ AppCompatActivity/app/src/main/java/com/gauravssnl/appcompat/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gauravssnl.appcompat;
+
+import android.app.*;
+import android.os.*;
+import android.support.v7.app.*;
+
+public class MainActivity extends AppCompatActivity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
