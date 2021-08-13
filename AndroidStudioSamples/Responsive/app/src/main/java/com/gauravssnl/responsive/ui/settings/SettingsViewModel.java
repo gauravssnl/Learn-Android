e4cffd8487e3a6af31cb048b34707a593a6d97ac
Gaurav Kumar <gaurav.ssnl@gmail.com>
2021-08-14 00:43:49 +0530

    Add Android Studio sample projects.

---
+++ AndroidStudioSamples/Responsive/app/src/main/java/com/gauravssnl/responsive/ui/settings/SettingsViewModel.java
@@ -0,0 +1,19 @@
+package com.gauravssnl.responsive.ui.settings;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SettingsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SettingsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is settings fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
