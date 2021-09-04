4571643e4b8111909dffeee45190be4926f4cec8
Gaurav <gauravssnl@gmail.com>
2021-09-04 21:27:43 +0530

    Remove Android Studio Samples.

--- AndroidStudioSamples/FragmentPlusViewModel/app/src/main/java/com/gauravssnl/fragmentplusviewmodel/ui/main/MainViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.gauravssnl.fragmentplusviewmodel.ui.main;
-
-import androidx.lifecycle.ViewModel;
-
-public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
